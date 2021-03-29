5ad3c8850353356b19c75f01ffc6b2719c4a7325
Timothy Small <smalls.online@icloud.com>
2021-03-29 15:49:06 -0400

    Removed unused variable

--- src/AzureAD.MFA.Pwsh/AzureAD.MFA.Pwsh.psm1
+++ src/AzureAD.MFA.Pwsh/AzureAD.MFA.Pwsh.psm1
@@ -4,7 +4,6 @@ foreach ($item in $internalFunctions) {
     . "$($item.FullName)"
 }
 
-$functionsBefore = Get-ChildItem -Path "Function:\"
 $exportableFunctions = Get-ChildItem -Path ([System.IO.Path]::Combine($PSScriptRoot, "functions\external\")) -Recurse | Where-Object { $PSItem.Extension -eq ".ps1" }
 foreach ($item in $exportableFunctions) {
     . "$($item.FullName)"
