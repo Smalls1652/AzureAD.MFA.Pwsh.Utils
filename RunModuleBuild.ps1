7e03351b63e28c5999bba89d1bcb15a3d755e17a
Timothy Small <smalls.online@icloud.com>
2021-03-30 14:59:41 -0400

    Updated build script

--- RunModuleBuild.ps1
+++ RunModuleBuild.ps1
@@ -1,43 +1,70 @@
 [CmdletBinding()]
 param(
-
+    [Parameter(Position = 0)]
+    [ValidateSet("Debug", "Release")]
+    [string]$PublishType = "Debug"
 )
 
 $ScriptLocation = $PSScriptRoot
 
-$buildDir = [System.IO.Path]::Combine($ScriptLocation, "build\")
-$buildModuleDir = [System.IO.Path]::Combine($buildDir, "AzureAD.MFA.Pwsh\")
+$buildLogSplat = @{
+    "Tags" = @("BuildLog");
+    "InformationAction" = "Continue"
+}
+Write-Information @buildLogSplat -MessageData "Starting build for 'AzureAD.MFA.Pwsh'"
+Write-Information @buildLogSplat -MessageData "---------------------"
+Write-Information @buildLogSplat -MessageData "- Build location: '$($ScriptLocation)'"
 
-$pwshProjectDir = [System.IO.Path]::Combine($ScriptLocation, "src\", "AzureAD.MFA.Pwsh\")
+$buildDir = Join-Path -Path $ScriptLocation -ChildPath "build\"
+$buildModuleDir = Join-Path -Path $buildDir -ChildPath "AzureAD.MFA.Pwsh\"
 
-$csProjectDir = [System.IO.Path]::Combine($ScriptLocation, "src\", "AzureAD.MFA.Pwsh.Lib\")
-$csProjectPublishDir = [System.IO.Path]::Combine($csProjectDir, "bin\", "Debug\", "net5.0\", "publish\")
+$srcDir = Join-Path -Path $ScriptLocation -ChildPath "src\"
+$pwshProjectDir = Join-Path -Path $srcDir -ChildPath "AzureAD.MFA.Pwsh\"
+
+$csProjectDir = Join-Path -Path $srcDir -ChildPath "AzureAD.MFA.Pwsh.Lib\"
+$csProjectPublishDir = Join-Path -Path $csProjectDir -ChildPath "bin\$($PublishType)\net5.0\publish\"
 
 $filesToCopy = [System.Collections.Generic.List[string[]]]@(
-    ([System.IO.Path]::Combine($csProjectPublishDir, "AzureAD.MFA.Pwsh.dll"))
+    (Join-Path -Path $csProjectPublishDir -ChildPath "AzureAD.MFA.Pwsh.dll")
 )
 
+Write-Information @buildLogSplat -MessageData "- Moving to directory: '$($csProjectDir)'"
 Push-Location -Path $csProjectDir
 
 try {
-    dotnet clean
-    dotnet publish /property:PublishWithAspNetCoreTargetManifest=false
+    Write-Information @buildLogSplat -MessageData "`- Building class library: 'AzureAD.MFA.Pwsh.Lib'"
+    dotnet clean --nologo --verbosity minimal
+    dotnet publish --nologo --configuration "$($PublishType)" --verbosity minimal /property:PublishWithAspNetCoreTargetManifest=false
     #Start-Process -FilePath "dotnet" -ArgumentList @("clean") -Wait -NoNewWindow -ErrorAction Stop
     #Start-Process -FilePath "dotnet" -ArgumentList @("publish", "/property:PublishWithAspNetCoreTargetManifest=false") -Wait -NoNewWindow -ErrorAction Stop
 }
 finally {
+    Write-Information @buildLogSplat -MessageData "- Returning back to root project dir"
     Pop-Location
 }
 
 
 if (Test-Path -Path $buildDir) {
+    Write-Information @buildLogSplat -MessageData "- Cleaning up previous build"
     Remove-Item -Path $buildDir -Recurse -Force
 }
 
+Write-Information @buildLogSplat -MessageData "- Creating build directory"
 $null = New-Item -Type Directory -Path $buildDir
 
+Write-Information @buildLogSplat -MessageData "- Building module at: '$($buildDir)'"
+
+Write-Information @buildLogSplat -MessageData "`t- Copying '$($pwshProjectDir)'"
 Copy-Item -Path $pwshProjectDir -Destination $buildDir -Recurse
 
 foreach ($item in $filesToCopy) {
+    Write-Information @buildLogSplat -MessageData "`t- Copying '$($item)'"
     Copy-Item -Path $item -Destination $buildModuleDir
 }
+
+$builtModule = Get-Item -Path $buildModuleDir
+
+Write-Information @buildLogSplat -MessageData "Build complete!"
+Write-Information @buildLogSplat -MessageData "---------------------"
+
+Write-Output -InputObject $builtModule
