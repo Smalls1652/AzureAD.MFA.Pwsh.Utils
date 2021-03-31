cfee410748856e3ba6331ef05706409e179f9d5a
Timothy Small <smalls.online@icloud.com>
2021-03-31 13:00:49 -0400

    Added last sign in threshold parameter

--- src/AzureAD.MFA.Pwsh/functions/external/Get-AadUsersWithLicense.ps1
+++ src/AzureAD.MFA.Pwsh/functions/external/Get-AadUsersWithLicense.ps1
@@ -6,12 +6,14 @@ function Get-AadUsersWithLicense {
         [string]$UserDomainName,
         [Parameter(Position = 1)]
         [ValidateNotNullOrEmpty()]
-        [string]$SkuId = "e97c048c-37a4-45fb-ab50-922fbf07a370" #The default value is for the 'M365 A5 for Faculty' SKU.
+        [string]$SkuId = "e97c048c-37a4-45fb-ab50-922fbf07a370", #The default value is for the 'M365 A5 for Faculty' SKU.
+        [Parameter(Position = 2)]
+        [int]$LastSignInThresholdDays = 30
     )
 
     $null = Test-MsGraphModuleIsConnected -ErrorAction "Stop"
 
-    $lastSignInDateThreshold = [datetime]::Now.AddDays(-30) #There could be users who haven't signed in within the last month, so let's ignore them for now.
+    $lastSignInDateThreshold = [datetime]::Now.AddDays(-($LastSignInThresholdDays)) #There could be users who haven't signed in within the last month, so let's ignore them for now.
 
     <#
     Get users based off the following info:
@@ -22,8 +24,8 @@ function Get-AadUsersWithLicense {
     Write-Verbose "Getting users"
     $usersWithLicense = Get-MgUser -Filter "endsWith(userPrincipalName, '@$($UserDomainName)') and assignedLicenses/any(c:c/skuId eq $($SkuId)) and accountEnabled eq true" -All -ConsistencyLevel Eventual -CountVariable "count" -Property @("id", "userPrincipalName", "assignedLicenses", "signInActivity")
 
-    #Filter out users who haven't signed into their account in the last 30 days.
-    Write-Verbose "Filtering users who haven't signed-in in the last month."
+    #Filter out users who haven't signed into their account in the last nn days.
+    Write-Verbose "Filtering users who haven't signed-in in the last $($LastSignInThresholdDays) days."
     $usersWithLicense = $usersWithLicense | FilterByLastSignIn -Threshold $lastSignInDateThreshold
 
     return $usersWithLicense
